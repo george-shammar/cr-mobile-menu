5065c5c07dcb705cbc6f8ec3ad7fd1f14a1f6af4
george-shammar <george.gbenle@outlook.com>
2021-07-25 15:12:11 +0000

    Add on click event to menu button

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const ham = document.querySelector('.menu-bars');
 const closeIcon = document.querySelector('.x-bars');
 const links = document.querySelector('.desktop-nav');
+
+ham.onclick = () => {
+    ham.style.display = 'none';
+    closeIcon.style.display = 'block';
+    links.style.display = 'block';
+};
+  
